e0c81f5f5ea81b21c9466c9c2b53ebd18fe24dd3
Mattias Lundell <mattias@lundell.com>
2024-04-08 15:34:09 +0200

    tests: async functions

--- tests/test_async_functions.py
+++ tests/test_async_functions.py
@@ -1,142 +1,164 @@
 import pytest
 from opperai import AsyncClient
 from opperai.types import CacheConfiguration, ChatPayload, FunctionDescription, Message
-
-DEFAULT_NAME = "test_function"
-DEFAULT_DESCRIPTION = "Test function"
-DEFAULT_INSTRUCTIONS = "Do something"
+from contextlib import asynccontextmanager
 
 
-@pytest.fixture
-async def function(
-    request: pytest.FixtureRequest,
-    aclient: AsyncClient,
-):
-    name = DEFAULT_NAME
-    description = DEFAULT_DESCRIPTION
-    instructions = DEFAULT_INSTRUCTIONS
-
-    try:
-        name = request.param.get("name", DEFAULT_NAME)
-        description = request.param.get("description", DEFAULT_DESCRIPTION)
-        instructions = request.param.get("instructions", DEFAULT_INSTRUCTIONS)
-    except AttributeError:
-        pass
-
-    fdesc = FunctionDescription(
-        path=f"test/sdk/{name}",
-        description=description,
-        instructions=instructions,
-    )
-
-    return await aclient.functions.create(fdesc)
+@asynccontextmanager
+async def fn(desc: FunctionDescription, c: AsyncClient):
+    fid = await c.functions.create(desc)
+    yield fid
+    await c.functions.delete(id=fid)
 
 
 @pytest.mark.asyncio(scope="module")
-async def test_create_function(function, vcr_cassette):
-    fid = await function
-    assert fid is not None
+async def test_create_function(vcr_cassette, aclient: AsyncClient):
+    async with fn(
+        FunctionDescription(
+            path="test/sdk/test_create_function_async",
+            description="Test function",
+            instructions="Do something",
+        ),
+        aclient,
+    ) as fid:
+        assert fid is not None
 
 
-@pytest.mark.parametrize("function", [{"name": "test_get_by_id"}], indirect=True)
 @pytest.mark.asyncio(scope="module")
-async def test_get_by_id(aclient: AsyncClient, function, vcr_cassette):
-    fid = await function
-    f_by_id = await aclient.functions.get_by_id(fid)
-    assert f_by_id.path == "test/sdk/test_get_by_id"
+async def test_get_by_id(aclient: AsyncClient, vcr_cassette):
+    async with fn(
+        FunctionDescription(
+            path="test/sdk/test_get_by_id",
+            description="Test function",
+            instructions="Do something",
+        ),
+        aclient,
+    ) as fid:
+        f_by_id = await aclient.functions.get_by_id(fid)
+        assert f_by_id.path == "test/sdk/test_get_by_id"
 
 
-@pytest.mark.parametrize("function", [{"name": "test_get_by_path"}], indirect=True)
 @pytest.mark.asyncio(scope="module")
-async def test_get_by_path(aclient: AsyncClient, function, vcr_cassette):
-    await function
-    f_by_path = await aclient.functions.get_by_path("test/sdk/test_get_by_path")
-    assert f_by_path.path == "test/sdk/test_get_by_path"
+async def test_get_by_path(aclient: AsyncClient, vcr_cassette):
+    async with fn(
+        FunctionDescription(
+            path="test/sdk/test_get_by_path",
+            description="Test function",
+            instructions="Do something",
+        ),
+        aclient,
+    ) as _:
+        f_by_path = await aclient.functions.get_by_path("test/sdk/test_get_by_path")
+        assert f_by_path.path == "test/sdk/test_get_by_path"
 
 
-@pytest.mark.parametrize("function", [{"name": "test_get"}], indirect=True)
 @pytest.mark.asyncio(scope="module")
-async def test_get(aclient: AsyncClient, function, vcr_cassette):
-    fid = await function
-    f_by_id = await aclient.functions.get(id=fid)
-    assert f_by_id.path == "test/sdk/test_get"
+async def test_get(aclient: AsyncClient, vcr_cassette):
+    async with fn(
+        FunctionDescription(
+            path="test/sdk/test_get",
+            description="Test function",
+            instructions="Do something",
+        ),
+        aclient,
+    ) as fid:
+        f_by_id = await aclient.functions.get(id=fid)
+        assert f_by_id.path == "test/sdk/test_get"
 
-    f_by_id = await aclient.functions.get(path="test/sdk/test_get")
-    assert f_by_id.path == "test/sdk/test_get"
+        f_by_id = await aclient.functions.get(path="test/sdk/test_get")
+        assert f_by_id.path == "test/sdk/test_get"
 
 
-@pytest.mark.parametrize("function", [{"name": "test_update_function"}], indirect=True)
 @pytest.mark.asyncio(scope="module")
-async def test_update_function(aclient: AsyncClient, function, vcr_cassette):
-    fid = await function
-    f = await aclient.functions.get_by_id(fid)
-    f.instructions = "Do something else"
-    fn_id = await aclient.functions.update(f)
-    f1 = await aclient.functions.get_by_id(fn_id)
+async def test_update_function(aclient: AsyncClient, vcr_cassette):
+    async with fn(
+        FunctionDescription(
+            path="test/sdk/test_update_function",
+            description="Test function",
+            instructions="Do something",
+        ),
+        aclient,
+    ) as fid:
+        f = await aclient.functions.get_by_id(fid)
+        f.instructions = "Do something else"
+        fn_id = await aclient.functions.update(f)
+        f1 = await aclient.functions.get_by_id(fn_id)
 
     assert f1.instructions == "Do something else"
 
 
-@pytest.mark.parametrize(
-    "function", [{"name": "test_delete_function_by_id"}], indirect=True
-)
 @pytest.mark.asyncio(scope="module")
-async def test_delete_function_by_id(aclient: AsyncClient, function, vcr_cassette):
-    fid = await function
-    f = await aclient.functions.get(id=fid)
-    assert f is not None
-    await aclient.functions.delete(id=f.id)
-    f = await aclient.functions.get(id=f.id)
-    assert f is None
+async def test_delete_function_by_id(aclient: AsyncClient, vcr_cassette):
+    async with fn(
+        FunctionDescription(
+            path="test/sdk/test_delete_function_by_id",
+            description="Test function",
+            instructions="Do something",
+        ),
+        aclient,
+    ) as fid:
+        f = await aclient.functions.get(id=fid)
+        assert f is not None
+        await aclient.functions.delete(id=f.id)
+        f = await aclient.functions.get(id=f.id)
+        assert f is None
 
 
-@pytest.mark.parametrize(
-    "function", [{"name": "test_delete_function_by_path"}], indirect=True
-)
 @pytest.mark.asyncio(scope="module")
-async def test_delete_function_by_path(aclient: AsyncClient, function, vcr_cassette):
-    fid = await function
-    f = await aclient.functions.get(id=fid)
-    assert f is not None
-    await aclient.functions.delete(path=f.path)
-    f = await aclient.functions.get(path=f.path)
-    assert f is None
+async def test_delete_function_by_path(aclient: AsyncClient, vcr_cassette):
+    async with fn(
+        FunctionDescription(
+            path="test/sdk/test_delete_function_by_path",
+            description="Test function",
+            instructions="Do something",
+        ),
+        aclient,
+    ) as fid:
+        f = await aclient.functions.get(id=fid)
+        assert f is not None
+        await aclient.functions.delete(path=f.path)
+        f = await aclient.functions.get(path=f.path)
+        assert f is None
 
 
-@pytest.mark.parametrize(
-    "function",
-    [{"name": "test_async_chat", "instructions": "translate to french"}],
-    indirect=True,
-)
 @pytest.mark.asyncio(scope="module")
-async def test_async_chat(function, aclient: AsyncClient, vcr_cassette):
-    fid = await function
-    f = await aclient.functions.get_by_id(fid)
-    resp = await aclient.functions.chat(
-        f.path, ChatPayload(messages=[Message(role="user", content="hello")])
-    )
+async def test_async_chat(aclient: AsyncClient, vcr_cassette):
+    async with fn(
+        FunctionDescription(
+            path="test/sdk/test_async_chat",
+            description="Translate to French",
+            instructions="Translate to French",
+        ),
+        aclient,
+    ) as fid:
+        f = await aclient.functions.get_by_id(fid)
+        resp = await aclient.functions.chat(
+            f.path, ChatPayload(messages=[Message(role="user", content="hello")])
+        )
 
-    assert resp.message == "Bonjour"
+        assert "bonjour" in resp.message.lower()
 
 
-@pytest.mark.parametrize(
-    "function",
-    [{"name": "test_async_chat_stream", "instructions": "translate to french"}],
-    indirect=True,
-)
 @pytest.mark.asyncio(scope="module")
-async def test_async_chat_stream(function, aclient: AsyncClient, vcr_cassette):
-    fid = await function
-    f = await aclient.functions.get_by_id(fid)
-    gen = await aclient.functions.chat(
-        f.path,
-        ChatPayload(messages=[Message(role="user", content="hello")]),
-        stream=True,
-    )
+async def test_async_chat_stream(aclient: AsyncClient, vcr_cassette):
+    async with fn(
+        FunctionDescription(
+            path="test/sdk/test_async_chat_stream",
+            description="Translate to French",
+            instructions="Translate to French",
+        ),
+        aclient,
+    ) as fid:
+        f = await aclient.functions.get_by_id(fid)
+        gen = await aclient.functions.chat(
+            f.path,
+            ChatPayload(messages=[Message(role="user", content="hello")]),
+            stream=True,
+        )
 
-    resp = "".join([message.delta async for message in gen if message.delta])
+        resp = "".join([message.delta async for message in gen if message.delta])
 
-    assert resp == "Bonjour"
+        assert "bonjour" in resp.lower()
 
 
 @pytest.mark.asyncio(scope="module")
@@ -148,22 +170,23 @@ async def test_create_function_with_cache(aclient: AsyncClient, vcr_cassette):
         cache_configuration=CacheConfiguration(exact_match_cache_ttl=10),
     )
 
-    fid = await aclient.functions.create(fdesc)
-    assert fid is not None
+    try:
+        fid = await aclient.functions.create(fdesc)
+        assert fid is not None
 
-    res = await aclient.functions.chat(
-        fdesc.path, ChatPayload(messages=[Message(role="user", content="hello")])
-    )
-    print(res)
-    assert not res.cached
+        res = await aclient.functions.chat(
+            fdesc.path, ChatPayload(messages=[Message(role="user", content="hello")])
+        )
+        print(res)
+        assert not res.cached
 
-    res = await aclient.functions.chat(
-        fdesc.path, ChatPayload(messages=[Message(role="user", content="hello")])
-    )
-    print(res)
-    assert res.cached
-
-    await aclient.functions.delete(path=fdesc.path)
+        res = await aclient.functions.chat(
+            fdesc.path, ChatPayload(messages=[Message(role="user", content="hello")])
+        )
+        print(res)
+        assert res.cached
+    finally:
+        await aclient.functions.delete(path=fdesc.path)
 
 
 @pytest.mark.asyncio(scope="module")
@@ -174,27 +197,27 @@ async def test_create_function_with_cache_flush(aclient: AsyncClient, vcr_casset
         instructions="Do something",
         cache_configuration=CacheConfiguration(exact_match_cache_ttl=10),
     )
+    try:
+        fid = await aclient.functions.create(fdesc)
+        assert fid is not None
 
-    fid = await aclient.functions.create(fdesc)
-    assert fid is not None
+        res = await aclient.functions.chat(
+            fdesc.path, ChatPayload(messages=[Message(role="user", content="hello")])
+        )
+        print(res)
+        assert not res.cached
 
-    res = await aclient.functions.chat(
-        fdesc.path, ChatPayload(messages=[Message(role="user", content="hello")])
-    )
-    print(res)
-    assert not res.cached
+        res = await aclient.functions.chat(
+            fdesc.path, ChatPayload(messages=[Message(role="user", content="hello")])
+        )
+        print(res)
+        assert res.cached
 
-    res = await aclient.functions.chat(
-        fdesc.path, ChatPayload(messages=[Message(role="user", content="hello")])
-    )
-    print(res)
-    assert res.cached
-
-    await aclient.functions.flush_cache(id=fid)
-    res = await aclient.functions.chat(
-        fdesc.path, ChatPayload(messages=[Message(role="user", content="hello")])
-    )
-    print(res)
-    assert not res.cached
-
-    await aclient.functions.delete(path=fdesc.path)
+        await aclient.functions.flush_cache(id=fid)
+        res = await aclient.functions.chat(
+            fdesc.path, ChatPayload(messages=[Message(role="user", content="hello")])
+        )
+        print(res)
+        assert not res.cached
+    finally:
+        await aclient.functions.delete(path=fdesc.path)
