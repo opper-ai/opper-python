a07f1cfe4da2879ddc6529b81bef910c6730b061
gosa <goran@gsandahl.net>
2024-04-22 13:41:33 +0200

    Add types and other improvements

--- src/opperai/functions/_async_functions.py
+++ src/opperai/functions/_async_functions.py
@@ -88,36 +88,37 @@ class AsyncFunctions:
     async def create(
         self, function: Function, update: bool = True, **kwargs
     ) -> Function:
-        """ Create an Opper function. 
-        
+        """ Create or update an Opper function.
+
         Args:
-            function: An object of type Function, representing the configuration of the function such as model.
-            update: If set to true, the existing function will be updated when a function with the same path already exists.
-            kwargs: Additional keyword arguments that will be included in the request payload.
+            function (Function): An instance of the Function class, representing the configuration
+                of the function to be created or updated. This includes details such as the function's
+                model, path, description, and any other relevant configuration parameters.
+            update (bool, optional): If True, the function will be updated if it already exists
+                based on its path. Defaults to True.
+            **kwargs: Additional keyword arguments that will be included in the request payload.
 
         Returns:
-            Returns Function object.
-        
-        Exceptions:
-            Throws an APIError if the creation or update fails.
+            Function: An instance of the Function class, representing the newly created or updated
+                function, including its server-assigned ID and any other relevant information.
+
+        Raises:
+            APIError: If the function creation or update fails due to an API error.
+            ValueError: If both `id` and `path` are provided, or neither is provided.
 
         Examples:
-            from opperai import Client
-            from opperai.types import Function
-
-            client = Client(api_key="opper_api_key")
-
-            function = Function(
-                path="test/function",
-                description="A function to translate text to French",
-                instructions="Translate the given text to French."
-            )
-
-            response = client.functions.create(function, update=True)
+            >>> from opperai import AsyncClient
+            >>> from opperai.types import Function
+            >>> client = AsyncClient(api_key="your_api_key_here")
+            >>> function = Function(
+            ...     path="example/function",
+            ...     description="A function to do something",
+            ...     model="gpt-3.5-turbo"
+            ... )
+            >>> created_function = asyncio.run(client.functions.create(function))
+            >>> print(created_function)
             
-            print(response)
-            
-            >>> id=93 path='test/function' description='A function to translate text to French' input_schema=None out_schema=None instructions='Translate the given text to French.' model='azure/gpt4-eu' index_ids=[] use_semantic_search=None few_shot=False few_shot_count=2 cache_configuration=None
+            id=93 path='test/function' description='A function to translate text to French' input_schema=None out_schema=None instructions='Translate the given text to French.' model='azure/gpt4-eu' index_ids=[] use_semantic_search=None few_shot=False few_shot_count=2 cache_configuration=None
         """
         fn = await self.get(path=function.path)
         if fn is None:
@@ -129,26 +130,33 @@ class AsyncFunctions:
 
     @validate_id_xor_path
     async def delete(self, id: str = None, path: str = None):
-        """ Delete an Opper function by its id or path
-        
+        """ Delete an Opper function by its ID or path.
+
+        This method allows for the deletion of a function either by specifying its unique ID or its path. 
+        If the deletion is successful, the method returns True. If the function cannot be found or the deletion 
+        fails, the method returns False.
+
         Args:
-            id: Id of the function to delete
-            path: Path of the function to delete
-         
+            id (str, optional): The unique identifier of the function to delete. Defaults to None.
+            path (str, optional): The path of the function to delete. Defaults to None.
+
         Returns:
-            True if successful, else False
-        
+            bool: True if the function was successfully deleted, False otherwise.
+
+        Raises:
+            ValueError: If both `id` and `path` are provided, or if neither is provided.
+
         Examples:
-            from opperai import Client
-            from opperai.types import Function
+            >>> from opperai import AsyncClient
 
-            client = Client(api_key="opper_api_key")
+            >>> client = AsyncClient(api_key="opper_api_key")
+            >>> response = asyncio.run(client.functions.delete(path="test/function"))
+            >>> print(response)
+            True
 
-            response = client.functions.delete(path="test/function")
-
-            print(response)
-            
-            >>> True
+        Note:
+            It's important to provide either `id` or `path`, but not both. If neither is provided, the method 
+            will raise a ValueError to indicate the issue.
         """
         
         if path is not None:
@@ -174,34 +182,59 @@ class AsyncFunctions:
     async def chat(
         self, function_path, data: ChatPayload, stream=False, **kwargs
     ) -> FunctionResponse:
-        """ Send messages to a specific Opper function and receive a response. 
-        
+        """ Send messages to a specific Opper function and receive a response.
+
+        This method allows sending a message or a series of messages to a specified Opper function,
+        identified by its path. The function processes the message(s) and returns a response. This
+        method can operate in two modes: default and streaming. In default mode, it sends all messages
+        and waits for a single response. In streaming mode, it yields incremental results as they become
+        available.
+
         Args:
-            function_path: The path identifier for the target Opper function. This should be obtained through the app interface or API.
-            data: An object of type ChatPayload, representing the content to be processed during the interaction.
-            stream: When set to True, initializes a generator that yields incremental results. Defaults to False for single message exchanges.
+            function_path (str): The path identifier for the target Opper function. This should be
+                obtained through the app interface or API.
+            data (ChatPayload): An object of type ChatPayload, representing the content to be processed
+                during the interaction. It encapsulates the messages to be sent to the function.
+            stream (bool, optional): When set to True, initializes a generator that yields incremental
+                results as StreamingChunk objects. Defaults to False for single message exchanges.
+            **kwargs: Additional keyword arguments that will be passed to the underlying HTTP request.
 
         Returns:
-            Returns FunctionResponse by default. With stream=True it yields StreamingChunk generator.  
+            Union[FunctionResponse, Generator[StreamingChunk, None, None]]: By default, returns a
+            FunctionResponse object containing the function's response to the input messages. If
+            `stream` is True, it yields a generator of StreamingChunk objects, each representing a
+            portion of the function's response.
 
         Examples:
-            import os
-            from opperai import Client
-            from opperai.types import ChatPayload, Message
+            >>> import asyncio
+            >>> from opperai import AsyncClient
+            >>> from opperai.types import ChatPayload, Message
 
-            client = Client(api_key=os.environ["OPPER_API_KEY"])
+            >>> client = AsyncClient(api_key="your_api_key_here")
 
-            response = client.functions.chat(
-                "test/function",
-                ChatPayload(
-                    messages=[Message(role="user", content="hello")]
-                )
-            )
-
-            print(response.message)
-            
+            >>> response = asyncio.run(client.functions.chat(
+            ...    "test/function",
+            ...    ChatPayload(
+            ...        messages=[Message(role="user", content="hello")]
+            ...    )
+            ... ))
             >>> print(response.message)
-            Bonjour    
+            'Bonjour'
+
+            # Example with streaming
+            >>> async def stream_chat():
+            ...     async for chunk in client.functions.chat(
+            ...         "test/function",
+            ...         ChatPayload(messages=[Message(role="user", content="hello")]),
+            ...         stream=True
+            ...     ):
+            ...         print(chunk.message)
+            >>> asyncio.run(stream_chat())
+
+        Note:
+            The `stream` parameter allows for real-time interaction with the function, which can be
+            particularly useful for functions designed to process input incrementally or maintain a
+            conversation state.
         """
 
         if data.parent_span_uuid is None:
