9cca2e9ceea7094b1b63acc55759d08a7828db29
Johnny Chadda <johnny@chadda.se>
2024-09-24 16:12:13 +0200

    Read version from pyproject.toml

--- src/opperai/__version__.py
+++ src/opperai/__version__.py
@@ -1,6 +1,13 @@
-from importlib.metadata import version, PackageNotFoundError
+import toml
 
-try:
-    __version__ = version("opperai")
-except PackageNotFoundError:
-    __version__ = "unknown"
+
+def get_version_from_pyproject():
+    try:
+        with open("pyproject.toml", "r") as f:
+            pyproject_data = toml.load(f)
+            return pyproject_data["tool"]["poetry"]["version"]
+    except (FileNotFoundError, KeyError):
+        return "unknown"
+
+
+__version__ = get_version_from_pyproject()
